02b095c48a1247e8c73a95a713aa5525c0c2cb50
RobinBigus <robin.bigus@wbscodingschool.com>
2025-12-01 13:51:08 +0100

    Added file-upload solution

---
+++ module09-restful-apis/06-exercises/file-upload/backend/src/db.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+try {
+  if (!process.env.MONGO_URI) throw new Error('Missing MONGO_URI');
+  await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
+  console.log('MongoDB connected');
+} catch (error) {
+  console.error('MongoDB connection error:', error);
+  process.exit(1);
+}
